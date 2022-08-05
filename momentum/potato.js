17c4bdea00f5921cb8b881411a255a7c3b2896dc
pudding-2 <hhs887488@gmail.com>
2022-08-05 18:30:53 +0900

    아섹스하고싶다

--- momentum/potato.js
+++ momentum/potato.js
@@ -1,47 +1,22 @@
-// let firstNum = prompt("첫번째 숫자");
-// let secondNum = prompt("두번째 숫자");
-// let dustkswk = prompt("어떤걸 하고싶냐냥", "더하기 뺴기 곱하기 나누기 제곱");
+const colors = document.querySelector("#text h1");
 
-// function rPtksrl(firstNum, secondNum, dustkswk) {
-//     firstNum = Number(firstNum);
-//     secondNum = Number(secondNum);
-//     if (dustkswk == "더하기"){
-//         document.write(firstNum + secondNum);
-//     }
-//     else if (dustkswk == "빼기"){
-//         document.write(firstNum - secondNum);
-//     }
-//     else if (dustkswk == "곱하기"){
-//         document.write(firstNum * secondNum);
-//     }
-//     else if (dustkswk == "나누기"){
-//         document.write(firstNum / secondNum);
-//     }
-//     else if (dustkswk == "제곱"){
-//         document.write(firstNum ** secondNum);
-//     }
-// }
-// rPtksrl(firstNum, secondNum, dustkswk);
-
-const rPtksrl = {
-    pl:function(a , b){
-    console.log(a + b)
-    },
-    min:function(a , b){
-    console.log(a - b)
-    },
-    multi:function(a , b){
-    console.log(a * b)
-    },
-    div:function(a , b){
-    console.log(a / b)
-    },
-    pow:function(a , b){
-    console.log(a ** b)
+function handleTitleClick() {
+    const wide = window.innerWidth;
+    if (wide > 200 && wide <= 500){
+        document.body.style.backgroundColor = "purple";
+    }
+    else if (wide <= 200){
+        document.body.style.backgroundColor = "blue";
+    }
+    else if (wide > 500){
+        document.body.style.backgroundColor = "beige";
     }
 }
-rPtksrl.pl(34, 35);
-rPtksrl.min(29, 5);
-rPtksrl.multi(7, 1);
-rPtksrl.div(69, 10);
-rPtksrl.pow(0, 0);
+
+window.onresize = handleTitleClick;
+
+
+
+
+
+
